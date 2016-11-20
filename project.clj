67fa19c7755e6456efc913c2cbf06ec3365e7d8e
evavro <me@madhax.io>
2016-11-20 10:56:28 -0800

    :package: added random data libs (faker, data.generator)

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://enron.madhax.io"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"] [org.clojure/tools.cli "0.3.5"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [org.clojure/data.generators "0.1.2"]
+                 [faker "0.2.2"]]
   :main ^:skip-aot enron.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
